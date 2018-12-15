4292b6006e357ee4bba581865c8f1cf7604bdcd6
JHirakawa211 <jhirakawa211@gmail.com>
2018-12-15 20:03:15 +0900

    init

---
+++ src/main/java/dfm/tool/tool/ToolApplication.java
@@ -0,0 +1,12 @@
+package dfm.tool.tool;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ToolApplication {
+
+	public static void main(String[] args) {
+		ObjectMapper mapper = new ObjectMapper();
+	}
+
+}
+
